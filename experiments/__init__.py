9702ba9241093b1bd29c5352e5800dbe8e4e9cc8
Shahzaib Mahar <shahzaibroshan.mahar@gmail.com>
2026-08-03 23:47:40 +0500

    init: create experiments module structure

---
+++ experiments/__init__.py
@@ -0,0 +1,9 @@
+"""
+DriftWatch Experiments Module
+
+Educational scripts for understanding statistical drift detection:
+- PSI (Population Stability Index)
+- KS Test (Kolmogorov-Smirnov)
+- Chi-Squared Test
+- Jensen-Shannon Divergence
+"""
